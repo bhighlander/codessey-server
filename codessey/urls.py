0fa77774898f473431535ea7eca84623c8a72c96
bhighlander <saphrimangel@gmail.com>
2023-09-06 14:31:30 -0500

    started work on register

--- codessey/urls.py
+++ codessey/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls  import include
+from rest_framework import routers
+from codesseyapi.views import register_user
+
+router = routers.DefaultRouter(trailing_slash=False)
 
 urlpatterns = [
+    path('register', register_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]

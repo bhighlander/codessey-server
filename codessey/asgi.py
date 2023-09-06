582584c92526b7619d798436090d9d176143c0a5
bhighlander <saphrimangel@gmail.com>
2023-09-06 10:56:27 -0500

    initial setup of django server

---
+++ codessey/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for codessey project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'codessey.settings')
+
+application = get_asgi_application()
---
+++ codesseyapi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CodesseyapiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'codesseyapi'

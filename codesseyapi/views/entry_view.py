62c7b187067fbe9be59d11b00a4e705c31ddd647
bhighlander <saphrimangel@gmail.com>
2023-09-07 14:28:27 -0500

    feat: view post details

--- codesseyapi/views/entry_view.py
+++ codesseyapi/views/entry_view.py
@@ -1,19 +1,29 @@
+from datetime import datetime
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework import serializers
-from datetime import datetime
+from rest_framework import serializers, status
 from codesseyapi.models import Entry, Programmer
 
 class EntryView(ViewSet):
     def list(self, request):
+        try:
+            entries = Entry.objects.order_by('-publication_date')
+            if "user" in request.query_params:
+                author = Programmer.objects.get(user=request.auth.user)
+                entries = entries.filter(author=author)
 
-        entries = Entry.objects.order_by('-publication_date')
-        if "user" in request.query_params:
-            author = Programmer.objects.get(user=request.auth.user)
-            entries = entries.filter(author=author)
-
-        serializer = EntrySerializer(entries, many=True)
-        return Response(serializer.data)
+            serializer = EntrySerializer(entries, many=True)
+            return Response(serializer.data)
+        except Entry.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+    
+    def retrieve(self, request, pk=None):
+        try:
+            entry = Entry.objects.get(pk=pk)
+            serializer = EntrySerializer(entry)
+            return Response(serializer.data)
+        except Entry.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
     
     def create(self, request):
         new_entry = Entry()

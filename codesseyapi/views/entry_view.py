9cba81468d08f60aababfe734ce1840787c6a12f
bhighlander <saphrimangel@gmail.com>
2023-09-08 18:06:28 -0500

    feat: add category to post

--- codesseyapi/views/entry_view.py
+++ codesseyapi/views/entry_view.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from codesseyapi.models import Entry, Programmer
+from rest_framework.decorators import action
+from codesseyapi.models import Entry, Programmer, Category
 
 class EntryView(ViewSet):
     def list(self, request):
@@ -16,7 +17,7 @@ class EntryView(ViewSet):
             return Response(serializer.data)
         except Entry.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-    
+
     def retrieve(self, request, pk=None):
         try:
             entry = Entry.objects.get(pk=pk)
@@ -24,7 +25,7 @@ class EntryView(ViewSet):
             return Response(serializer.data)
         except Entry.DoesNotExist as ex:
             return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
-    
+
     def create(self, request):
         new_entry = Entry()
         new_entry.title = request.data["title"]
@@ -37,10 +38,50 @@ class EntryView(ViewSet):
 
         serializer = EntrySerializer(new_entry, context={'request': request})
         return Response(serializer.data)
-    
+
+    def update(self, request, pk):
+
+        if "category_add" in request.data:
+            entry = Entry.objects.get(pk=pk)
+            categories = Category.objects.get(pk=request.data["category_add"])
+            entry.categories.add(categories) #set resets it, add adds to it, remove removes it. try add_or_remove or something like that? see if you can combine with _or_
+        elif "category_remove" in request.data:
+            categories = Category.objects.get(pk=request.data["category_remove"])
+            entry.categories.remove(categories)
+        else:
+            categories = request.data["categories"]
+            entry = Entry.objects.get(pk=pk)
+            entry.title = request.data["title"]
+            entry.content = request.data["content"]
+            entry.solved = request.data["solved"]
+
+        entry.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, _, pk=None):
+        try:
+            entry = Entry.objects.get(pk=pk)
+            entry.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Entry.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+
+class ProgrammerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Programmer
+        fields = ('id', 'full_name')
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'label')
 
 class EntrySerializer(serializers.ModelSerializer):
+    categories = CategorySerializer(many=True)
+    author = ProgrammerSerializer(many=False)
     class Meta:
         model = Entry
-        fields = ('id', 'title', 'content', 'author', 'publication_date', 'solved')
+        fields = ('id', 'title', 'content', 'author', 'publication_date', 'solved', 'categories')
         depth = 1

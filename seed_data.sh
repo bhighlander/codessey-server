ac810a1bdf3cd5f557fddb8545524157c63a33af
bhighlander <saphrimangel@gmail.com>
2023-09-19 16:25:34 -0500

    updated comments fixtures

--- seed_data.sh
+++ seed_data.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-rm db.sqlite3
-rm -rf ./codesseyapi/migrations
-python manage.py makemigrations codesseyapi
-python manage.py migrate
-python manage.py loaddata users
-python manage.py loaddata tokens
-python manage.py loaddata programmers
 python manage.py loaddata categories
 python manage.py loaddata entries
 python manage.py loaddata comments
 python manage.py loaddata todos
+
+# run seed_users, register new user, then run seed_data
